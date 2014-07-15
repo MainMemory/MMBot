212d1ea83f8b34a324c68289f1a2c9a29750782d
Karen Tsai <angelXwind@angelxwind.net>
2014-07-14 23:22:39 -0400

    Moved Twitter API authentication to a separate file, TwitterCredentials.cs which is under gitignore.

---
+++ MMBotTwitter/TwitterCredentials.example.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Timers;
+using LinqToTwitter;
+using MMBot;
+using Newtonsoft.Json;
+
+namespace MMBotTwitter
+{
+    public partial class TwitterModule : BotModule
+    {
+        //const string ConsumerKey = null; // The Consumer Key you get from registering an app on https://dev.twitter.com/
+        //const string ConsumerSecret = null; // The Consumer Secret you get from registering the app
+    }
+}
